c2e42953e2089df70a5013866938c235f509f112
janelim0414 <117237502+janelim0414@users.noreply.github.com>
2024-05-08 23:01:19 -0400

    client

---
+++ App.jsx
@@ -0,0 +1,117 @@
+import React, { useEffect, useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import axios from 'axios';
+import SearchBar from './SearchBar';
+
+function App() {
+    // fetch noteslist data from db
+    const [noteslist, setNotes] = useState([{}]);
+    const [searchResults, setSearchResults] = useState([]);
+    useEffect(() => {
+        axios
+        .get('http://localhost:4000/notes')
+        .then ((res) => {
+            setNotes(res.data);
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+    }, []);
+
+    const handleSearch = (query) => {
+        const results = noteslist.filter((note) =>
+            note.title.toLowerCase().includes(query.toLowerCase())
+        );
+        setSearchResults(results);
+    };
+
+    // incoming note in the input field
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
+    // // list of notes (intial list from notes.js)
+    // const [noteslist, setNotes] = useState(notes);
+
+    function handleTitle(event) {
+        setTitle(event.target.value);
+    }
+
+    function handleContent(event) {
+        setContent(event.target.value);
+    }
+
+    // send post request to backend
+    function handleClick(event) {
+        const newNote = {
+            title: title,
+            content: content
+        }
+        const newNotes = [...noteslist, newNote];
+
+        axios
+        .post('http://localhost:4000/notes', newNote)
+        .then((res) => {
+            setNotes(newNotes);
+        })
+        .catch((e) => {
+            console.log(e);
+        })
+        
+        // reset the form field
+        setTitle("");
+        setContent("");
+    }
+
+    function deleteNote(id) {
+        console.log(id);
+        axios
+        .delete(`http://localhost:4000/notes/${id}`)
+        .then((res) => {
+            const filtered = noteslist.filter((note) => note._id !== id);
+            setNotes(filtered);
+        })
+        .catch((e) => {
+            console.error(e);
+        })
+        
+    }
+
+    return (
+        <div>
+            <Header />
+            <SearchBar onSearch={handleSearch} />
+            <form className="form">
+            <input 
+            placeholder="Title" 
+            onChange={handleTitle}
+            value={title}/>
+            <textarea 
+            placeholder="Take a note..."
+            onChange={handleContent}
+            value={content}/>
+            <button className="button" onClick={handleClick}>
+                Add
+            </button>
+            </form>
+            <div>
+                {(searchResults.length > 0 ? searchResults : noteslist).map((note) => {
+                    return (
+                        <Note
+                        key={note._id}
+                        id={note._id}
+                        title={note.title}
+                        content={note.content}
+                        delete={() => deleteNote(note._id)}
+                        />
+                    )
+                })}
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
+
